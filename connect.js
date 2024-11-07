e0a6a3fd678997776f41bd6a153aecbc7c5e94e9
Ashwin GG Prabhu <gg.ashwin.prabhu@sap.com>
2024-11-07 21:34:15 +0530

    added sharans connection URI

--- connect.js
+++ connect.js
@@ -4,18 +4,27 @@ const { Sequelize } = require("sequelize");
 const { DataTypes, Model } = require("sequelize");
 const bcrypt = require("bcrypt");
 
-const sequelize = new Sequelize("BOOKING_DB", "postgres", "postgres", {
-  host: "localhost",
-  dialect: "postgres",
-});
+// Using the full service URI
+const sequelize = new Sequelize(
+  "postgres://avnadmin:AVNS_PmgRMTaa5tS65ZyUGw8@pg-hotel-service-hotelservice.k.aivencloud.com:19582/Hotel",
+  {
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // This option allows self-signed certificates
+      },
+    },
+  }
+);
 
 async function connectAndSync() {
   try {
     await sequelize.authenticate();
     console.log("Connection has been established successfully.");
     // You can call sync on your models here if you want
-    // await User.sync({ alter: true });
-    // await Payment.sync({ alter: true });
+    await User.sync({ alter: true });
+    await Payment.sync({ alter: true });
   } catch (error) {
     console.error("Unable to connect to the database:", error);
   }
