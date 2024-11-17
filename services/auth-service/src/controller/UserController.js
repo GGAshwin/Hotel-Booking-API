b508de98d00173bbfef513ce0e9934b4058afa45
Ashwin GG Prabhu <gg.ashwin.prabhu@sap.com>
2024-11-17 22:54:08 +0530

    fixed connect path

--- services/auth-service/src/controller/UserController.js
+++ services/auth-service/src/controller/UserController.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const { User } = require("../../../../connect");
+const { User } = require("../../connect");
 const verifyToken = require("../middlewares/auth");
 const { JWT_SECRET } = require("../constant");
 
